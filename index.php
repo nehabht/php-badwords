8a80c9b7762cdccbe00c68e55fc17b370c0c0c82
nehabht <ne.bhatti2@icloud.com>
2022-05-19 16:57:38 +0200

    add cesored word

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
+
 $quoteDany = "Fire cannot kill a dragon.";
+
+//var_dump($_GET['name']);
+
 
 
 
@@ -16,6 +20,37 @@ $quoteDany = "Fire cannot kill a dragon.";
 </head>
 <body>
     
+    <!-- stampare a schermo la stringa creata -->
     <h1><?php echo $quoteDany ?></h1>
+
+    <!-- stampare il numeri di caratteri nella stringa  -->
+    <p>
+        numero di caratteri nella stringa: 
+        <?php echo strlen($quoteDany)?>
+    </p>
+
+
+
+    <!-- Una parola da censurare viene passata dall'utente tramite parametro GET. -->
+    <div>
+        <h5>testo censurato:</h5>
+        <p>
+        <?php
+            $textCensored = str_replace($_GET['censura'], '***', $quoteDany );
+            echo $textCensored
+        ?>
+        </p>
+    </div>
+
+    <!-- lunghezza dopo censura -->
+    <p>
+        numero di caratteri nella stringa: 
+        <?php echo strlen($textCensored)?>
+    </p>
+
+
+
+
+
 </body>
 </html>
